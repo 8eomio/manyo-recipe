7467f9ef6363833341d54700f7d1a16ee82afb49
BeomJo Kim <61742009+8eomio@users.noreply.github.com>
2024-06-30 03:03:10 +0900

    Update Chat.tsx

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import styles from '../styles/Chat.module.css';
 import OpenAI from 'openai';
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY});
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY,dangerouslyAllowBrowser: true });
 
 interface ChatLog {
     type: 'user' | 'bot';
