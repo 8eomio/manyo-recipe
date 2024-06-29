5035226959e73f7e1e7b1d4d161656fdcf118b1b
8eomio <aidbeomjo@ajou.ac.kr>
2024-06-29 11:12:40 +0900

    try catch

--- app/api/refrigerator/route.ts
+++ app/api/refrigerator/route.ts
@@ -1,23 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
-import dummyigrd from '@/data/dummy.json'
-import { fetchIngredients, addIngredients} from "@/data/firestore"
+import { fetchIngredients, addIngredients } from "@/data/firestore";
+
 export async function GET(request: NextRequest) {
+  try {
     const fetchedIngredients = await fetchIngredients();
-    console.log("test")
+    console.log("test");
     const response = {
-        message: "재료 가져오기",
-        data: fetchedIngredients
-    }   
-    return NextResponse.json(response, {status: 201});
+      message: "재료 가져오기",
+      data: fetchedIngredients
+    };
+    return NextResponse.json(response, { status: 201 });
+  } catch (error) {
+    console.error("Error fetching ingredients:", error);
+    return NextResponse.json({ message: "Error fetching ingredients" }, { status: 500 });
   }
+}
 
 export async function POST(request: NextRequest) {
+  try {
     const { ingredient, userid } = await request.json();
     console.log("test post", userid);
-    const addedIngredients = await addIngredients({ingredient, userid});
+    const addedIngredients = await addIngredients({ ingredient, userid });
     const response = {
-        message: "성공",
-        data: addedIngredients
-    }
-    return Response.json(response, {status: 201});
+      message: "성공",
+      data: addedIngredients
+    };
+    return NextResponse.json(response, { status: 201 });
+  } catch (error) {
+    console.error("Error adding ingredient:", error);
+    return NextResponse.json({ message: "Error adding ingredient" }, { status: 500 });
   }
+}
